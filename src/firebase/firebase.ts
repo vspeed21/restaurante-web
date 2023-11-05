eb0a79c568ef52727458396603789633482bd634
vspeed21 <vtorresquintanilla0206@gmail.com>
2023-11-05 00:48:02 -0600

    add: firestore to firebase class

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,9 +1,15 @@
-import { initializeApp } from 'firebase/app';
+import app from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+
 import firebaseConfig from './config';
 
-class Firebase {
+export class Firebase {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  db: any
+
   constructor() {
-    initializeApp(firebaseConfig);
+    app.initializeApp(firebaseConfig);
+    this.db = app.firestore();
   }
 }
 
